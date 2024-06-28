cf8cd07e35a830dea1edcbd7dfb53f2c708b2728
Dastan <satybekovdastan@gmail.com>
2024-06-28 17:31:05 +0600

    Dev merge (#15)
    Buttu

--- DispatchTest/ViewController.swift
+++ DispatchTest/ViewController.swift
@@ -11,9 +11,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tvoi()
+        print("moi")
+        moi()
     }
 
+    func tvoi() {
+        print("poi")
+        print("feature moi ne trogaite")
+    }
+
+    func moi() {
+        print("anu 2")
+    }
+    
+    func tretii() {
+        print("Prikol")
+    }
+    
+    func fourth() {
+        print("fourth dva tri ")
+    }
 
 }
 
